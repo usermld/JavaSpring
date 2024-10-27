d236bb2b6485340aa090caf6c9e4d71f1b58d4b9
MU <m.iouriev@gmail.com>
2024-10-27 23:34:21 +0700

    initial

---
+++ src/main/rep/UserReposirory.java
@@ -0,0 +1,26 @@
+import ru.geekbrains.ProjectHW3.domain.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserRepository {
+
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+
+
+    private List<User> users = new ArrayList<>();
+
+}
